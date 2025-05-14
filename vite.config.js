87e2508500a04707da3dd8b130806107a708c808
owoseni1623 <owoseni0816@gmail.com>
2025-05-14 04:06:48 -0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: 'localhost',
+    port: 3000, // You can specify a specific port or remove this line to use the default
     proxy: {
       '/api': {
         target: 'https://grant-api.onrender.com',
         changeOrigin: true,
+        secure: false
       }
     }
   }
